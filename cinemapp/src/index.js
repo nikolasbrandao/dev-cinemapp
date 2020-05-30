695f88caca79d32b9a1dbf44fbc359dea69fc7da
nikolasbrandao <nikolasbrandao@gmail.com>
2020-05-29 22:40:27 -0300

    feat: init project

---
+++ cinemapp/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StatusBar} from 'react-native';
+
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#000" />
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  </>
+);
+
+export default App;
