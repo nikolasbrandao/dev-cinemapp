c8fa5bae32cf6fe7eafaeb11f6007a1cc28fe379
nikolasbrandao <nikolasbrandao@gmail.com>
2020-05-30 14:45:31 -0300

    feat: add favorite movies list  and adjust style

--- cinemapp/src/pages/Favorites/index.js
+++ cinemapp/src/pages/Favorites/index.js
@@ -2,18 +2,27 @@ import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
 import {Container, Title, SubTitle, CardMovie} from '../../components';
 import {CinemaService} from '../../services';
+import * as S from './styles';
 
 const Favorites = () => {
-  const [lading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [movieList, setMovieList] = useState([]);
 
+  const getMoviesData = async () => {
+    setLoading(true);
+    try {
+      const movies = await CinemaService.getAllMovies();
+      setMovieList(movies || []);
+      console.log(movies);
+    } catch (error) {
+      console.log('Error', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    CinemaService.getAllMovies()
-      .then((movies) => {
-        console.log('movies', movies);
-        setMovieList(movies || []);
-      })
-      .catch((err) => console.log('Error', err));
+    getMoviesData();
   }, []);
 
   const handleFavoriteButton = (item) => {
@@ -33,16 +42,20 @@ const Favorites = () => {
     <Container>
       <Title>Favoritos</Title>
       <SubTitle>Seus filmes e séries do coração</SubTitle>
-      <FlatList
-        data={movieList}
-        renderItem={({item}) => (
-          <CardMovie
-            key={item.imdbID}
-            movie={item}
-            onPressFavorite={handleFavoriteButton}
-          />
-        )}
-      />
+      <S.ListMovieWrapper>
+        <FlatList
+          data={movieList}
+          refreshing={loading}
+          onRefresh={() => getMoviesData()}
+          renderItem={({item}) => (
+            <CardMovie
+              key={item.imdbID}
+              movie={item}
+              onPressFavorite={handleFavoriteButton}
+            />
+          )}
+        />
+      </S.ListMovieWrapper>
     </Container>
   );
 };
